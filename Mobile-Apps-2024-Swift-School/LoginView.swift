65fc242a28bcfa33447e9e3e570144997ca16a6b
Paritosh Vaghasiya <Vaghasiya1923@students.d211.org>
2024-11-18 08:19:53 -0600

    Day 1

--- Mobile-Apps-2024-Swift-School/LoginView.swift
+++ Mobile-Apps-2024-Swift-School/LoginView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var errorMessage = ""
+    @State private var isLoggedIn = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack(spacing: 16){
+                HStack{
+                    Image(systemName: "envelope.fill")
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                }
+                HStack{
+                    Image(systemName: "lock.fill")
+                    SecureField("Password", text: $password)
+                }
+                if errorMessage.isEmpty == false {
+                    Text(errorMessage).foregroundColor(.red)
+                }
+                
+                Button("Login"){
+                    Task{
+                        await login()
+                    }
+                }
+                .disabled(email.isEmpty || password.isEmpty)
+            }
+            .padding()
+            .navigationTitle("Login")
+            .navigationDestination(isPresented: $isLoggedIn){
+                MainView()
+            }
+        }
+    }
+    
+    func login() async {
+        //Fill later
     }
 }
 
--- Mobile-Apps-2024-Swift-School/MainView.swift
+++ Mobile-Apps-2024-Swift-School/MainView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct MainView: View {
+    //@State private var users = [UserModel]()
+    @State private var errorMessage = ""
+    @State private var showLoginView = false
+    @State private var showUpdateUserView = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                Text("Welcome to the Main Screen")
+                if errorMessage.isEmpty{
+                    Text(errorMessage).foregroundColor(.red)
+                }
+                List(users){ user in
+                    VStack(alignment: .leading){
+                        Text("Name: \(user.firstName) \(user.lastName)")
+                        Text("City: \(user.city)")
+                        Text("Email: \(user.email)")
+                    }
+                }
+                .onAppear{
+                    Task {
+                        await fetchUsers()
+                    }
+                }
+                HStack{
+                    Button("Update User"){
+                        showUpdateUserView = true
+                    }
+                    .padding()
+                    .background(Color.blue.opacity(0.2))
+                    .cornerRadius(10)
+                    
+                    Button("Logout"){
+                        Task{
+                            await logout()
+                        }
+                    }
+                    .padding()
+                    .background(Color.blue.opacity(0.2))
+                    .cornerRadius(10)
+                }
+                .padding()
+                .navigationDestination(isPresented: $showLoginView){
+                    LoginView()
+                }
+                .navigationDestination(isPresented: $showUpdateUserView){
+                    UpdateUserView(showUpdateUserView: $showUpdateUserView)
+                }
+            }
+            .navigationTitle("User Details")
+        }
+    }
+    
+    func fetchUsers() async{
+        //Fill later
+    }
+    
+    func logout() async{
+        //Fill later
     }
 }
 
